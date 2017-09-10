ee37317712b9b4a42fd7bfcc468dbce59fd4a1f1
crafter <adam@crafter.net.pl>
2017-09-10 20:30:08 +0200

    exception new php 7

--- task1/catchable_exception.php
+++ task1/catchable_exception.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: crafter
- * Date: 10.09.2017
- * Time: 20:13
- */
+
+function hello_from($user) {
+    return $user->say_hello();
+}
+
+//php 5.6
+function hello_from2($user) {
+    if (is_null($user)) {
+        throw new Exception("User is null");
+    }
+    return $user->say_hello();
+}
+
+try {
+    echo hello_from2(null);
+
+} catch (Exception $e) {
+    echo "ssss: {$e->getMessage()}";
+}
+
+
+try {
+    echo hello_from(null);
+} catch (Error $e) {
+    echo "<br />Rescued from: {$e->getMessage()}";
+}
