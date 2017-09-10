f916095704fb20180a6d3dfdaf820746c1cf61d8
crafter <adam@crafter.net.pl>
2017-09-10 20:14:44 +0200

    dsaf

---
+++ task1/catchable_exception.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crafter
+ * Date: 10.09.2017
+ * Time: 20:13
+ */
--- task1/uniform_variable_syntax.php
+++ task1/uniform_variable_syntax.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: crafter
- * Date: 10.09.2017
- * Time: 11:19
- */
+$car = 'person';
+$person = 'Adam';
+
+echo $car; //person
+
+echo $$car; //Adam
+
+$name = 'car';
+$car  = ['name' => 'Jetta','year' => '2017'];
+
+var_dump($$name);
+
+echo $$name['name']; //php 5 return error
+echo $$name['name']; //php 7 return Jetta
+
+$name = 'car2';
+$car2 = [
+    'type' => [
+        'brand' => 'Volkswagen','model'=>'Jetta'
+    ],
+    'year'=>'2010'
+];
+echo '<pre>'; var_dump($car2); echo '</pre>';
+
+echo $$name['type']['model'];
+
+$test = 'test';
+$stdclass = new stdClass();
+$stdclass->$test = ['name'=>'adam'];
+
+echo $stdclass->$test['name'];
